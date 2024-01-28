e79b3beb7c72e5c5db29d0f65fc198ca3856764e
Ashish Vasant Vade <115925803+ashishvade@users.noreply.github.com>
2024-01-28 22:17:34 +0530

    commit

--- demo/src/main/java/com/example/demo/cloudOptima/sys/service/EmployeeService.java
+++ demo/src/main/java/com/example/demo/cloudOptima/sys/service/EmployeeService.java
@@ -149,8 +149,8 @@ public class EmployeeService {
         for (Employee employee : attendanceRecords) {
             EmployeeReport report = new EmployeeReport(
                 employee.getEmployeeId(),
-                employee.getName(),         // Provide name from Employee
-                employee.getDepartment(),   // Provide department from Employee
+                employee.getName(),         
+                employee.getDepartment(),  
                 employee.getLastAttendanceDate(),
                 employee.getCheckInTime(),
                 employee.getCheckOutTime(),
